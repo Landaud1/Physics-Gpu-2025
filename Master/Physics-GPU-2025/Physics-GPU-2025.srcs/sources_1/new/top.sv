83851a06da1efc1d420b3bde87e589fdb9bb8ba4
DJ Landau <landaud1@navi.lions.tcnj.edu>
2024-09-26 18:19:27 -0400

    Added phys engine and position ram to top

--- Master/Physics-GPU-2025/Physics-GPU-2025.srcs/sources_1/new/top.sv
+++ Master/Physics-GPU-2025/Physics-GPU-2025.srcs/sources_1/new/top.sv
@@ -6,8 +6,12 @@ module top(
     );
     
     
+    // General Signals
+    logic VSYNC;
+    
+    
     // Host I/F to state ram signals
-    // Nick you will be using these
+    // Nick you will be using these!
     logic [511:0] SRA_WRITE_DATA; // SRA stands for state ram side A
     logic [511:0] SRA_READ_DATA; // Might look into deleting, host i/f can't read
     logic [9:0] SRA_ADDR;
@@ -22,7 +26,22 @@ module top(
     logic SRB_EN; // Change to always enabled?
     
     
+    // Physics engine to position ram signals
+    logic [20:0] PRA_WRITE_DATA; // SRA stands for state ram side A
+    logic [9:0] PRA_ADDR;
+    logic PRA_WE;
+    logic PRA_EN;
+    
+    // state ram to physics engine signals
+    // Francis you will be using these!
+    logic [20:0] PRB_READ_DATA; 
+    logic [9:0] PRB_ADDR;
+    logic PRB_WE;
+    logic PRB_EN;
+    
+    
     // State ram:  connects between host I/F and physics engine
+    // Holds data about ping/pong positions, velocities, and masses of objects
     state_ram sr(
         .addra(SRA_ADDR),
         .addrb(SRB_ADDR),
@@ -39,5 +58,36 @@ module top(
         .web(SRB_WE),
         .ena(SRA_EN),
         .enb(SRB_EN)
+    );
+    
+    // Physics Engine
+    physics_engine pe(
+        .VSYNC(VSYNC),
+        .reset(RESET),
+        .clk(CLK),
+        
+        .SR_ADDR(SRB_ADDR),
+        .SR_READ(SRB_READ_DATA),
+        .SR_WRITE(SRB_WRITE_DATA),
+        
+        .PR_ADDR(PRA_ADDR),
+        .PR_WRITE(PRA_WRITE_DATA)
+    );
+    
+    // Position ram: connects between physics engine and render engine
+    // Holds data about positions of objects
+    position_ram pr(
+        .addra(PRA_ADDR),
+        .addrb(PRB_ADDR),
+        
+        .dina(PRA_WRITE_DATA),
+        
+        .doutb(PRB_READ_DATA),
+        
+        .clka(CLK),
+        .clkb(CLK),
+        .wea(PRA_WE),
+        .ena(PRA_EN),
+        .enb(PRB_EN)
     );
 endmodule
