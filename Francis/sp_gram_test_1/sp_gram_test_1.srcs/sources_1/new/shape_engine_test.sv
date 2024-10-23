b59f1d05d18abf0a5a7eda969ce8dd57b9380bc5
franbinoaldrino <faldridge071@gmail.com~>
2024-10-23 17:35:21 -0400

    Continued process of building shape engine

--- Francis/sp_gram_test_1/sp_gram_test_1.srcs/sources_1/new/shape_engine_test.sv
+++ Francis/sp_gram_test_1/sp_gram_test_1.srcs/sources_1/new/shape_engine_test.sv
@@ -1,13 +1,19 @@
 `timescale 1ns / 1ps
 
 module shape_engine_test(
-    input  logic        clk,
-    input  logic        reset,
-    input  logic        set_mem,
-    input  logic        start_op
-    );
+    input logic clk,
+    input logic reset,
+    input logic set_mem,
+    input logic start_op
+);
 
-
+    logic write_enable = 0;
+    logic read_enable = 0;
+    logic [19:0] address_write = '0;
+    logic [19:0] address_read ='0;
+    logic [3:0] data_in = '0;
+    logic [3:0] data_out = '0;
+    
     gram gram(
         .clka(clk),
         .wea(write_enable),
@@ -19,101 +25,193 @@ module shape_engine_test(
         .doutb(data_out)
     );
     
+    logic write_enable_ab = 0;
+    logic read_enable_ab = 0;
+    logic [3:0] address_write_ab = 1'h0;
+    logic [3:0] address_read_ab = 1'h0;
+    logic [32:0] data_in_ab = 1'h0;
+    logic [32:0] data_out_ab = 1'h0;
+    
     // Made an oopsie making this block, should be 32x10 but who cares
-    abram simple_33_x_10_ABRAM(
+    simple_33_x_10_ABRAM abram(
         .clka(clk),
-        .wea(write_enable),
+        .wea(write_enable_ab),
         .addra(address_write_ab),
         .dina(data_in_ab),
         .clkb(clk),
-        .enb(read_enable),
+        .enb(read_enable_ab),
         .addrb(address_read_ab),
         .doutb(data_out_ab)
     );
     
-    logic [9:0] x_mem = null;
-    logic [9:0] y_mem = null;
-    logic [9:0] x_coord = null;
-    logic [9:0] y_coord = null;
-    logic [9:0] x_dev = 0;
-    logic [9:0] y_dev = 0;
-    logic [9:0] curr_n = 0;
+    parameter int SCREEN_WIDTH = 1280;
+    parameter int SCREEN_HEIGHT = 720;
+    parameter int RAM_SIZE = SCREEN_WIDTH * SCREEN_HEIGHT;  // 921,600 slots
+    parameter int N_OBJECTS = 4;
     
-    logic [9:0] n = 0; 
-    logic [9:0] n_pointer = 0;
-    logic [4:0] size_x = 0;
-    logic [4:0] size_y = 0;
-    logic [4:0] color = 0;
+    // State machine registers
+    typedef enum logic [2:0] {IDLE, SET_MEM, READ_OBJ, PROCESS, UPDATE_DEV, DONE} state_t;
+    state_t state, next_state;
     
-
-    logic [3:0] state_set_mem = 0;
-    logic [3:0] state = 0;
+    // Registers 
+    logic [9:0] x_coord, y_coord;  
+    logic [3:0] x_size, y_size;     
+    logic [3:0] color;
+    logic [9:0] curr_n;
+    logic [3:0] x_dev, y_dev;
     
-    
-    // Always comb block to modify n_pointer
-    always_comb begin
-        address_read = n;
-        x_coord = data_out_ab[31:22];
-        y_coord = data_out_ab[21:12];
-        size_x = data_out_ab[11:8];
-        size_y = data_out_ab[7:4];
-        color = data_out_ab[3:0];
-    end
-    
-    // Always ff block to set mem for testing; mimics job of Host Int
-    always_ff @ (posedge clk) begin
+    // State machine logic
+    always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            state_set_mem <= 0;
-        end else begin
-            case (state_set_mem)
-                0:  if (set_mem == 1) begin
-                        state_mem = 1;
-                    end
-                1: begin
-                    write_enable <= 1'b1;
-                    address_write_mem <= 4'b0;
-                    /*  x_coord = 0
-                        y_coord = 0
-                        size_x = 1;
-                        size_y = 1;
-                        color = 1010;
-                    */
-                    data_in_mem <= 33'b000000000000000000000000100011010;
-                    state_mem <= 4'h2;
-                end 
-                2: begin
-                    address_write_mem <= 4'b1;
-                    /*  x_coord = 4
-                        y_coord = 3
-                        size_x = 2;
-                        size_y = 2;
-                        color = 1111;
-                    */
-                    data_in_mem <= 33'b000000001000000000100001000101111;
-                    state_mem <= 4'h3;
-                end
-                3: begin
-                    n <= 2; // Change this depending on n
-                end               
-            end
-        end
-endmodule
-
-// Always ff block for filling gram based on abram
-always_ff @ (posedge clk) begin
-        if (reset) begin
-            x_mem <= null;
-            y_mem <= null;
-            x_coord <= null;
-            y_coord <= null;
+            state <= IDLE;
+            curr_n <= 0;
             x_dev <= 0;
             y_dev <= 0;
-            curr_n <= 0;
-            state <= 0;
         end else begin
-            case (state)
-                0:  if (start_op == 1) begin
-                        state = 1;
-                    end
-                1: begin
-                
+            state <= next_state;
+        end
+    end
+    
+    // Next state logic
+    logic [1:0] buffer = 0;
+    
+    always_comb begin
+        case (state)
+            IDLE:
+                if (set_mem) next_state = SET_MEM;
+                else next_state = IDLE;
+            SET_MEM:
+                if (start_op) next_state = READ_OBJ;
+                else next_state = SET_MEM;
+            READ_OBJ:
+                if (curr_n == N_OBJECTS) next_state = DONE;
+                else begin
+                    if (buffer == 2) next_state = PROCESS;
+                    else next_state = READ_OBJ;
+                end
+            PROCESS:
+                next_state = UPDATE_DEV;
+            UPDATE_DEV:
+                if (curr_pixel >= num_pixels) next_state = READ_OBJ;
+                else next_state = PROCESS;
+            DONE:
+                next_state = DONE;
+        endcase
+    end
+    
+    logic [19:0] mem_adr;
+    logic [5:0] num_pixels;
+    logic [5:0] curr_pixel = 0;
+    always_comb begin
+        // Decode data, maintain mem_adr
+        mem_adr = (1280 * (y_coord + y_dev)) + (x_coord + x_dev);
+        x_coord = data_out_ab[32:22];
+        y_coord = data_out_ab[21:12];
+        x_size = data_out_ab[11:8];
+        y_size = data_out_ab[7:4];
+        color = data_out_ab[3:0];
+        num_pixels = x_size * y_size;
+    end
+    
+    logic switch = 0;
+    
+    always_ff @(posedge clk) begin
+        if (state == SET_MEM) begin
+            if (curr_n < N_OBJECTS && switch == 0) begin
+                write_enable_ab <= 1;
+                address_write_ab <= curr_n;
+                case (curr_n)
+                    0:
+                     /* x_coord = 0
+                        y_coord = 1
+                        x_size = 1
+                        y_size = 3
+                        color = 6
+                     */ 
+                        data_in_ab <= 33'b000000000000000000001000100110110;
+                    1:
+                     /* x_coord = 3
+                        y_coord = 0
+                        x_size = 3
+                        y_size = 1
+                        color = 8
+                     */ 
+                        data_in_ab <= 33'b000000000110000000000001100011000;
+                    2:
+                     /* x_coord = 5
+                        y_coord = 2
+                        x_size = 2
+                        y_size = 2
+                        color = 9
+                     */ 
+                        data_in_ab <= 33'b000000001010000000010001000101001;
+                    3:
+                     /* x_coord = 3
+                        y_coord = 3
+                        x_size = 1
+                        y_size = 1
+                        color = 3
+                     */ 
+                        data_in_ab <= 33'b000000000110000000011000100010011;
+                endcase
+                curr_n <= curr_n + 1;
+            end else begin
+                write_enable_ab <= 0;
+                switch <= 1;
+                curr_n <= 0;
+            end
+        end
+    end
+    
+    // Read object details from the object RAM
+    always_ff @(posedge clk) begin
+        if (state == READ_OBJ) begin
+            if (buffer == 0) begin
+                address_read_ab <= curr_n;
+                read_enable_ab <= 1;
+            end
+            buffer <= buffer + 1;
+            x_dev <= 0;
+            y_dev <= 0;
+        end else begin
+            buffer <= 0;
+        end
+    end
+    
+    always_ff @(posedge clk) begin
+        if (state == PROCESS) begin
+            write_enable <= 1;
+            address_write <= mem_adr;
+            data_in <= color; 
+        end
+    end
+    
+    always_ff @(posedge clk) begin
+        if (state == UPDATE_DEV) begin
+            if (x_size == 1 && y_size != 1) begin
+                // Case: vertical line
+                y_dev <= y_dev + 1;
+            end else if (x_size != 1 && y_size == 1) begin
+                // Case: horizontal line
+                x_dev <= x_dev + 1;
+            end else begin
+                // Case: at least 1x1, 2x2+ rectangle (everything else)
+                if (x_dev == x_size - 1) begin
+                    x_dev <= 0;
+                    y_dev <= y_dev + 1;
+                end else 
+                    x_dev <= x_dev + 1;
+            end
+            
+            curr_pixel <= curr_pixel + 1;
+            if (curr_pixel == num_pixels) begin
+                curr_n = curr_n + 1;
+                write_enable <= 0;
+                curr_pixel <= 0;
+            end
+        end
+        
+    end
+            
+
+endmodule
--- Francis/sp_gram_test_1/sp_gram_test_1.srcs/sources_1/new/testbench.sv
+++ Francis/sp_gram_test_1/sp_gram_test_1.srcs/sources_1/new/testbench.sv
@@ -5,6 +5,7 @@ module testbench();
     logic        clk;
     logic        reset;
     logic        start_op;
+    logic        set_mem;
     
     // instantiate top
     read_write_test rwt(clk, reset);
@@ -12,13 +13,25 @@ module testbench();
     blank_fill_test bft(clk, reset, start_op);
     
     vid_time_counter_test vtct(clk, reset, start_op);
+    
+    shape_engine_test set(
+        .clk(clk),
+        .reset(reset),
+        .set_mem(set_mem),
+        .start_op(start_op)
+    );
+
 
     // reset signal
     initial begin
         reset = 1;
+        set_mem = 0;
+        start_op = 0;
         #88 reset = 0;
-        start_op = 1;
+        set_mem = 1;
+        #88 start_op = 1;
     end
+    
         
     // initiate clock
     initial begin
