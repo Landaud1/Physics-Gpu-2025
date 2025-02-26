c86200f8f744d7198faa35b538361ecf8757849a
franbinoaldrino <faldridge071@gmail.com~>
2025-02-26 13:54:47 -0500

    please

---
+++ Francis/sp_gram_test_2/project_2/project_2.srcs/sources_1/new/mem_functions.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2025 06:21:47 PM
+// Design Name: 
+// Module Name: mem_functions
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mem_functions(
+
+    );
+endmodule
