ca32d7c71c4af51d58d4fd3ef74530a489c76fd7
DJ Landau <landaud1@navi.lions.tcnj.edu>
2025-03-02 19:46:02 -0500

    implemented pixel walk, removed border test, added new top layer to make test arams/prams to begin shape engine work

--- Francis/sp_gram_test_2/project_2/project_2.srcs/sources_1/new/testbench.sv
+++ Francis/sp_gram_test_2/project_2/project_2.srcs/sources_1/new/testbench.sv
@@ -6,12 +6,20 @@ module testbench();
     logic        reset_n = 0;
     logic [3:0]  sw;
     logic        new_frame;
+    
+    logic [9:0] pram_write_adr;
+    logic [20:0] pram_write_data;
+    logic pram_wea;
 
     toptop toptop(
         .clk(clk_100),                 
         .reset_n(reset_n),
         .sw(sw),
-        .new_frame(new_frame)
+        .new_frame(new_frame),
+        
+        .pram_write_adr(pram_write_adr),
+        .pram_write_data(pram_write_data),
+        .pram_wea(pram_wea)
     );
     
     initial begin
@@ -27,7 +35,26 @@ module testbench();
     end
     
     initial begin
-        #1100 
+        wait(new_frame);
+        pram_wea = 1;
+        #10 pram_write_adr = 10'b0000000000;            // Obj 0
+        pram_write_data = 21'b0001100100_00001100100;   // y = 100, x = 100
+        #10 pram_write_adr = 10'b0000000001;            // Obj 1
+        pram_write_data = 21'b0011001000_00011001000;   // y = 200, x = 200
+        #10 pram_write_adr = 10'b0000000010;            // Obj 2
+        pram_write_data = 21'b0100101100_00100101100;   // y = 300, x = 300
+        #10 pram_wea = 0;
+        
+        wait(new_frame);
+        pram_wea = 1;
+        #10 pram_write_adr = 10'b0000000000;            // Obj 0
+        pram_write_data = 21'b0001100100_00001100100;   // y = 100, x = 100
+        #10 pram_write_adr = 10'b0000000001;            // Obj 1
+        pram_write_data = 21'b0011001000_00011001000;   // y = 200, x = 200
+        #10 pram_write_adr = 10'b0000000010;            // Obj 2
+        pram_write_data = 21'b0100101100_00100101100;   // y = 300, x = 300
+        #10 pram_wea = 0;
+        
     
     
     end
--- Francis/sp_gram_test_2/project_2/project_2.srcs/sources_1/new/toptop.sv
+++ Francis/sp_gram_test_2/project_2/project_2.srcs/sources_1/new/toptop.sv
@@ -5,11 +5,15 @@ module toptop(
     input logic     reset,
 
     input logic [3:0] sw,
-    output logic new_frame
+    output logic new_frame,
+    
+    input logic         pram_wea,
+    input logic [9:0]   pram_write_adr,
+    input logic [20:0]  pram_write_data
     );
     
     clk_wiz_0 clk_wiz_0(
-        .reset(reset),
+        .reset(~reset),
         .clk_in1(clk_100),
         .clk_out1(clk), // clk_74_25
         .locked(locked)
@@ -28,7 +32,12 @@ module toptop(
     );
     
     position_ram pram(
-        .newframe(new_frame),
+        .clk(clk),
+        .wea(pram_wea),
+        .reset(~reset),
+        .adr_write(pram_write_adr),
+        .data_write(pram_write_data),
+        .newframe(new_frame)
     );
     
 endmodule
